d0951ba9bb86707985a802123eefa7a5c2c7c1ea
Mike Stewart <thewonderidiot@gmail.com>
2019-04-22 09:02:14 -0700

    FPGA: corrected pin and general project updates

--- fpga/rotinom.runs/impl_1/rotinom.tcl
+++ fpga/rotinom.runs/impl_1/rotinom.tcl
@@ -61,12 +61,110 @@ proc step_failed { step } {
 }
 
 
+start_step init_design
+set ACTIVE_STEP init_design
+set rc [catch {
+  create_msg_db init_design.pb
+  create_project -in_memory -part xc7z020clg484-1
+  set_property board_part numato.com:styx:part0:1.0 [current_project]
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
+  set_property webtalk.parent_dir /home/mike/rotinom/fpga/rotinom.cache/wt [current_project]
+  set_property parent.project_path /home/mike/rotinom/fpga/rotinom.xpr [current_project]
+  set_property ip_output_repo /home/mike/rotinom/fpga/rotinom.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
+  add_files -quiet /home/mike/rotinom/fpga/rotinom.runs/synth_1/rotinom.dcp
+  set_msg_config -source 4 -id {BD 41-1661} -limit 0
+  set_param project.isImplRun true
+  add_files /home/mike/rotinom/fpga/rotinom.srcs/sources_1/bd/rotinom_ps/rotinom_ps.bd
+  read_ip -quiet /home/mike/rotinom/fpga/rotinom.srcs/sources_1/ip/rope_memory/rope_memory.xci
+  read_ip -quiet /home/mike/rotinom/fpga/rotinom.srcs/sources_1/ip/prop_clk_div/prop_clk_div.xci
+  read_ip -quiet /home/mike/rotinom/fpga/rotinom.srcs/sources_1/ip/core_memory/core_memory.xci
+  set_param project.isImplRun false
+  read_xdc /home/mike/rotinom/fpga/rotinom.srcs/constrs_1/new/rotinom.xdc
+  set_param project.isImplRun true
+  link_design -top rotinom -part xc7z020clg484-1
+  set_param project.isImplRun false
+  write_hwdef -force -file rotinom.hwdef
+  close_msg_db -file init_design.pb
+} RESULT]
+if {$rc} {
+  step_failed init_design
+  return -code error $RESULT
+} else {
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force rotinom_opt.dcp
+  create_report "impl_1_opt_report_drc_0" "report_drc -file rotinom_drc_opted.rpt -pb rotinom_drc_opted.pb -rpx rotinom_drc_opted.rpx"
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
+    implement_debug_core 
+  } 
+  place_design 
+  write_checkpoint -force rotinom_placed.dcp
+  create_report "impl_1_place_report_io_0" "report_io -file rotinom_io_placed.rpt"
+  create_report "impl_1_place_report_utilization_0" "report_utilization -file rotinom_utilization_placed.rpt -pb rotinom_utilization_placed.pb"
+  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file rotinom_control_sets_placed.rpt"
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force rotinom_routed.dcp
+  create_report "impl_1_route_report_drc_0" "report_drc -file rotinom_drc_routed.rpt -pb rotinom_drc_routed.pb -rpx rotinom_drc_routed.rpx"
+  create_report "impl_1_route_report_methodology_0" "report_methodology -file rotinom_methodology_drc_routed.rpt -pb rotinom_methodology_drc_routed.pb -rpx rotinom_methodology_drc_routed.rpx"
+  create_report "impl_1_route_report_power_0" "report_power -file rotinom_power_routed.rpt -pb rotinom_power_summary_routed.pb -rpx rotinom_power_routed.rpx"
+  create_report "impl_1_route_report_route_status_0" "report_route_status -file rotinom_route_status.rpt -pb rotinom_route_status.pb"
+  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file rotinom_timing_summary_routed.rpt -pb rotinom_timing_summary_routed.pb -rpx rotinom_timing_summary_routed.rpx -warn_on_violation "
+  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file rotinom_incremental_reuse_routed.rpt"
+  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file rotinom_clock_utilization_routed.rpt"
+  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file rotinom_bus_skew_routed.rpt -pb rotinom_bus_skew_routed.pb -rpx rotinom_bus_skew_routed.rpx"
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force rotinom_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
+  unset ACTIVE_STEP 
+}
+
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  open_checkpoint rotinom_routed.dcp
-  set_property webtalk.parent_dir /home/mike/rotinom/fpga/rotinom.cache/wt [current_project]
   set_property XPM_LIBRARIES {XPM_CDC XPM_MEMORY} [current_project]
   catch { write_mem_info -force rotinom.mmi }
   write_bitstream -force rotinom.bit 
